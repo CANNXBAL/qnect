5a91e84b8163e310e7967de2086ae6613a1fb26d
CANNXBAL <cannxbal28@gmail.com>
2026-06-14 05:03:47 -0700

    Fix signup redirect

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -27,6 +27,7 @@ export default function SignupPage() {
       email,
       password,
       options: {
+        emailRedirectTo: `${window.location.origin}/login`,
         data: {
           username,
         },
